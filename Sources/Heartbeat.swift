9d13080c2af08d4ca386162884082a941dc5270a
Yuri Karabatov <yuri@yurikarabatov.com>
2016-01-03 01:38:23 +0300

    Simple heartbeat.

---
+++ Sources/Heartbeat.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+class Heartbeat {
+    /// Check that Stockfighter API is up.
+    class func isAPIup(handler: ((Bool) -> Void)?) {
+        guard let heartbeatURL = NSURL(string: "https://api.stockfighter.io/ob/api/heartbeat") else { 
+            handler?(false)
+            return
+        }
+
+        NSURLSession.sharedSession().dataTaskWithURL(heartbeatURL) { data, response, error in
+            guard error == nil else {
+                handler?(false)
+                return
+            }
+
+            handler?(true)
+        }.resume()
+    }
+}
