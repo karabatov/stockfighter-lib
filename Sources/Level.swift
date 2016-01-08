97c3e272231101bd28809c9d56a97369c587512e
Yuri Karabatov <yuri@yurikarabatov.com>
2016-01-08 23:39:18 +0300

    Typealiases and structs necessary for interfacing with GM API.

--- Sources/Level.swift
+++ Sources/Level.swift
@@ -10,3 +10,50 @@ public enum StockfighterLevel: Int, CustomStringConvertible {
         }
     }
 }
+
+/// Trading account.
+public typealias Account = String
+
+/// Level instance id, used to control a level lifecycle via the GM API.
+public typealias InstanceId = Int
+
+/// Human-readable level instructions.
+public typealias Instructions = [String: String]
+
+/// Stock symbol e.g. “FOOBAR”.
+public typealias StockSymbol = String
+
+/// Venue symbol e.g. “TESTEX”.
+public typealias VenueSymbol = String
+
+/// Currency symbol e.g. “USD”.
+public typealias CurrencySymbol = String
+
+/// Currency amount.
+public typealias CurrencyAmount = Int
+
+/// State of the level, "open" etc.
+public enum LevelState: String {
+    case Open = "open"
+}
+
+/// Contains properties for a game level which we need to be able to play it: instructions, trading account, etc.
+public struct Level {
+    let account: Account
+    let instance: InstanceId
+    let instructions: Instructions
+    let secondsPerDay: Int
+    let tickers: [StockSymbol]
+    let venues: [VenueSymbol]
+    let balances: [CurrencySymbol: CurrencyAmount]
+}
+
+/// Describes the current status of a level which we get through the GM API.
+public struct LevelStatus {
+    let totalDays: Int
+    let tradingDay: Int
+    let done: Bool
+    let instance: InstanceId
+    let state: LevelState
+    let date: NSDate
+}
